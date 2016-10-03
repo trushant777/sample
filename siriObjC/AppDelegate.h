411ff56ac02d2332df2b14f067b39b141080518f
“Sarang <your_email@youremail.com>
2016-10-03 22:07:24 +0530

    Initial Commit

---
+++ siriObjC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  siriObjC
+//
+//  Created by Sarang Jiwane on 03/10/16.
+//  Copyright © 2016 iDevelopers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ siriObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  siriObjC
+//
+//  Created by Sarang Jiwane on 03/10/16.
+//  Copyright © 2016 iDevelopers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
