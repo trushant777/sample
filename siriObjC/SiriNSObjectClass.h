76cf56363f5513ef32181e73be4063a7d1a508c1
trushant963 <trushant@esenseit.in>
2017-01-10 18:02:26 +0530

    first commit

---
+++ siriObjC/SiriNSObjectClass.h
@@ -0,0 +1,13 @@
+//
+//  SiriNSObjectClass.h
+//  siriObjC
+//
+//  Created by Sarang Jiwane on 13/10/16.
+//  Copyright © 2016 iDevelopers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SiriNSObjectClass : NSObject
+
+@end
---
+++ siriObjC/SiriNSObjectClass.m
@@ -0,0 +1,13 @@
+//
+//  SiriNSObjectClass.m
+//  siriObjC
+//
+//  Created by Sarang Jiwane on 13/10/16.
+//  Copyright © 2016 iDevelopers. All rights reserved.
+//
+
+#import "SiriNSObjectClass.h"
+
+@implementation SiriNSObjectClass
+
+@end
